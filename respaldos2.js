d90e081fdfc669ab5a62d4848cca56154e893d2f
MichelleArileni <michelle.balcazar.ortiz@gmail.com>
2020-01-16 12:49:10 -0600

    proyecto terminado

--- respaldos2.js
+++ respaldos2.js
@@ -1,39 +1,31 @@
 var mysqlDump = require('mysqldump');
-//npm install node-schedule --save
 var schedule = require('node-schedule');
 const moment = require('moment')
 const fs = require('fs')
 var CronJob = require('cron').CronJob
 
-/* Funcion general ASYNC que encapsule las funciones independientes */
-//async function todosRespaldos() {
 
-/* AREGLO DE BASES DE DATOS */
+/* ARREGLO DE BASES DE DATOS */
 const DBS = [
-    //'recargasatc',
-    //'recargasgeneral',
+    'recargasatc',
+    'recargasgeneral',
     'marquesadabd',
-    /*'recargasbmc',
+    'recargasbmc',
     'consignacion',
-    'recargasEnlace',
-    'recargasIdea',
-    'recargasSierra',
-    'recargasMacrocel',
-    'recargasOrticel',
-    'recargasPremium',
-    'recargasProcel',
-    'recargasTCC',
-    'recargasTGCEL' */
+    'recargasenlace',
+    'recargasidea',
+    'recargassierra',
+    'recargasmacrocel',
+    'recargasorticel',
+    'recargaspremium',
+    'recargasprocel',
+    'recargastcc',
+    'recargastgcel' 
 ]
-function recargasatc(){ 
-    
+ function recargasatc(){ 
+   schedule.scheduleJob('0 13 12 * * *', function(){
     for (i = 0; i < DBS.length; i++) {
-        let arreglo = DBS[i];
-        //console.log(arreglo)
-        
-        /* Funcion para atrapar los errores */
-        let long =  DBS.length
-        console.log(long)
+
         const fileName = `${DBS[i]}-${moment().format('YYYY-MM-DD')}.sql`
         const wstream = fs.createWriteStream(`./respaldos/${DBS[i]}/${fileName}`)
         
@@ -44,25 +36,17 @@ function recargasatc(){
             password: 'marquesada?466',
             database: DBS[i],
         },
-    
             /* Le pasamos a la funcion dump la ruta con el nombre del archivo ya armado */
             dumpToFile: wstream.path,
-            
         },
-    
             function(err){
                 console.log(error)
                 if (err) { console.log("error") }
                 else { console.log("Dump correcto desde models") }
             })
-            if(i == long){
-                exit
-            }
-        
-        
-    }
+        }
+    })
 }
-
 
 
 recargasatc()
